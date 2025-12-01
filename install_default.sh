ae1f8192fba5cb4778c58c4018667db76a78a445
João Gustavo <jgbispo20@gmail.com>
2025-12-01 21:44:01 +0000

    Remove icons

--- install_default.sh
+++ install_default.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-
 # install essential packages
 sudo pacman -Syu --noconfirm
 sudo pacman -S --needed base-devel --noconfirm
 sudo pacman -S eza git neovim zsh zoxide bat fd ripgrep starship procs --noconfirm
-sudo pacman -S ttf-fira-code ttf-jetbrains-mono noto-fonts-emoji --noconfirm
+sudo pacman -S nerd-fonts-jetbrains-mono --noconfirm
 
 # install yay (AUR helper)
 # check if yay is already installed
@@ -23,12 +22,14 @@ yay -Syu --noconfirm
 
 # Adicionando as config ao .bashrc
 
+rm -f ~/.bashrc
+
 echo 'export EDITOR=nvim' >> ~/.bashrc
 echo 'export VISUAL=nvim' >> ~/.bashrc
 
-echo 'alias ll="eza -lha --group-directories-first --icons"' >> ~/.bashrc
-echo 'alias la="eza -a --icons"' >> ~/.bashrc
-echo 'alias ls="eza --icons"' >> ~/.bashrc
+echo 'alias ll="eza -lha --group-directories-first"' >> ~/.bashrc
+echo 'alias la="eza -a"' >> ~/.bashrc
+echo 'alias ls="eza"' >> ~/.bashrc
 
 echo 'alias cat="bat --paging=never --style=plain"' >> ~/.bashrc
 
@@ -41,6 +42,9 @@ echo 'alias grep="rg"' >> ~/.bashrc
 echo 'eval "$(zoxide init bash)"' >> ~/.bashrc
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config/nvim/ 
+
+
+rm -f ~/.config/starship.toml
 
 echo 'add_newline = true' >> ~/.config/starship.toml
 echo 'format = "[$directory]($style) $git_branch $git_state $git_status"' >> ~/.config/starship.toml
@@ -56,6 +60,4 @@ echo 'modified = "●"' >> ~/.config/starship.toml
 echo 'staged = "✔"' >> ~/.config/starship.toml
 echo 'untracked = "★"' >> ~/.config/starship.toml 
 
-
-
-source ~/.bashrc
+source $HOME/.bashrc
